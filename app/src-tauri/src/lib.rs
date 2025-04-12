5250ccaf4f03087d1ef63e35735cdae85b4da745
aochan <nakinagaraodoru1013@gmail.com>
2025-04-12 16:04:13 +0900

    カーソルモード中にJキーを押したらカーソルを下に移動する

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -18,60 +18,129 @@ mod nin_core {
     pub const CONTROL_KEY: &str = "ctrl";
     #[allow(dead_code)]
     pub const SPACE_KEY: &str = "space";
+    #[allow(dead_code)]
+    pub const J_KEY: &str = "j";
+
+    const CURSOR_MODE_EVENT: &str = "cursor_mode";
+
+    const CURSOR_DOWN_EVENT: &str = "cursor_down";
+
+    #[derive(PartialEq, Copy, Clone)]
+    enum MODE {
+        IDLE,
+        CURSOR,
+    }
 
     #[allow(dead_code)]
     pub struct NinCore {
-        is_idle: bool,
-        is_cursor: bool,
+        mouse_controller: Box<dyn MouseController>,
+        mode: MODE,
     }
 
     impl NinCore {
         #[allow(dead_code)]
-        pub fn new() -> Self {
+        pub fn new(mouse_controller: impl MouseController + 'static) -> Self {
             Self {
-                is_idle: true,
-                is_cursor: false,
+                mouse_controller: Box::new(mouse_controller),
+                mode: MODE::IDLE,
             }
         }
 
         #[allow(dead_code)]
         pub fn is_idle(&self) -> bool {
-            self.is_idle
-        }
-
-        #[allow(dead_code)]
-        pub fn fire_key_event(&mut self, key1: &str, key2: &str) {
-            if key1 == CONTROL_KEY && key2 == SPACE_KEY {
-                self.is_cursor = true;
-            }
+            self.mode == MODE::IDLE
         }
 
         #[allow(dead_code)]
         pub fn is_cursor(&self) -> bool {
-            self.is_cursor
+            self.mode == MODE::CURSOR
         }
+
+        #[allow(dead_code)]
+        pub fn fire_key_event(&mut self, key1: &str, key2: &str) {
+            let event = self.to_event_from_keys(key1, key2, self.mode);
+
+            if event == CURSOR_MODE_EVENT {
+                self.mode = MODE::CURSOR;
+            }
+
+            if event == CURSOR_DOWN_EVENT {
+                self.mouse_controller.move_cursor(0, 10);
+            }
+        }
+
+        // @TODO これを別structにして、NinCoreがショートカットキーの詳細を知らなくてもよくする
+        fn to_event_from_keys(&self, key1: &str, key2: &str, mode: MODE) -> String {
+            if key1 == CONTROL_KEY && key2 == SPACE_KEY {
+                return CURSOR_MODE_EVENT.to_string()
+            }
+
+            if mode == MODE::CURSOR && key1 == J_KEY {
+                return CURSOR_DOWN_EVENT.to_string()
+            }
+
+            "".to_string()
+        }
+    }
+
+    pub trait MouseController {
+        fn move_cursor(&mut self, x: i32, y: i32);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::nin_core::{CONTROL_KEY, SPACE_KEY};
+    use crate::nin_core::{MouseController, NinCore, CONTROL_KEY, J_KEY, SPACE_KEY};
+    use mockall::{mock, predicate};
 
     use super::*;
 
+    mock! {
+        pub MockMouseController {}
+        impl MouseController for MockMouseController {
+            fn move_cursor(&mut self, x: i32, y: i32);
+        }
+    }
+
     #[test]
     fn nin_coreの起動時はアイドルモードになっている() {
-        let sut = nin_core::NinCore::new();
+        let mock = MockMockMouseController::new();
+        let sut = nin_core::NinCore::new(mock);
 
         assert_eq!(sut.is_idle(), true);
     }
 
     #[test]
     fn nin_coreはctrlとspaceを受け取ったらカーソルモードになる() {
-        let mut sut = nin_core::NinCore::new();
+        let mock = MockMockMouseController::new();
+        let mut sut = nin_core::NinCore::new(mock);
 
         sut.fire_key_event(CONTROL_KEY, SPACE_KEY);
 
         assert_eq!(sut.is_cursor(), true);
+    }
+
+    #[test]
+    fn nin_coreはカーソルモードでjを入力するとカーソル位置を10下に下げる指令を出す() {
+        let mut mock = MockMockMouseController::new();
+        mock.expect_move_cursor()
+            .with(predicate::eq(0), predicate::eq(10))
+            .times(1).returning(|_, _| ());
+
+        let mut nin = NinCore::new(mock);
+        nin.fire_key_event(CONTROL_KEY, SPACE_KEY);
+        nin.fire_key_event(J_KEY, "");
+    }
+
+    #[test]
+    fn nin_coreはカーソルモードでpを入力しても何もしない() {
+        let mut mock = MockMockMouseController::new();
+        mock.expect_move_cursor()
+            .times(0);
+
+        let mut sut = nin_core::NinCore::new(mock);
+
+        sut.fire_key_event(CONTROL_KEY, SPACE_KEY);
+        sut.fire_key_event("p", "");
     }
 }
